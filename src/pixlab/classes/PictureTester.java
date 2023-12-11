40d92ca65bea7d6d99a124409597cad046b29d9d
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-11 08:32:58 -0700

    Made test zero red method

--- src/pixlab/classes/PictureTester.java
+++ src/pixlab/classes/PictureTester.java
@@ -18,6 +18,14 @@ public class PictureTester
     beach.explore();
   }
   
+  public static void testZeroRed()
+  {
+	  Picture beach = new Picture("beach.jpg");
+	  beach.explore();
+	  beach.zeroRed();
+	  beach.explore();
+  }
+  
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
   {
@@ -59,7 +67,8 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    testZeroBlue();
+    //testZeroBlue();
+    testZeroRed();
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
