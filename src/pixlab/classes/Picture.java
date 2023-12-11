677f3ce8af2cb7d5d9d7c023077651a72c2cf8b5
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-11 08:28:04 -0700

    Added zero red method

--- src/pixlab/classes/Picture.java
+++ src/pixlab/classes/Picture.java
@@ -99,6 +99,19 @@ public class Picture extends SimplePicture
     }
   }
   
+  /** Method to set the red to 0 */
+  public void zeroRed()
+  {
+	  Pixel[][] pixels = this.getPixels2D();
+	  for (Pixel[] rowArray : pixels)
+	  {
+		  for (Pixel pixelObj : rowArray)
+		  {
+			  pixelObj.setRed(0);
+		  }
+	  }
+  }
+  
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
